db013641ec0c790f1b4bcc8fbc6a146426adef9c
Ignacio Arias <1992ignacio@gmail.com>
2024-01-25 16:26:52 -0800

    initial commit

---
+++ AllFastlane/AllFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllFastlaneApp.swift
+//  AllFastlane
+//
+//  Created by Ignacio Arias on 2024-01-25.
+//
+
+import SwiftUI
+
+@main
+struct AllFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
